d535e0f82ceb528fc8e1de3b999efe04511d958b
Your Name <you@example.com>
2022-09-22 19:54:48 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ *  _islower - check the code.
  *
- * Return: Always 0.
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
-int main(void)
-{
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
