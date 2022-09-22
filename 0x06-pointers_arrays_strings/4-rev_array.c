1460924df7ad2ebe07e65647ca7e8fd10ebfd6b9
Your Name <you@example.com>
2022-09-22 19:09:27 +0300

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,41 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
+ * reverse_array - Reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ * Return: None
  */
-void print_array(int *a, int n)
+
+void reverse_array(int *a, int n)
 {
-    int i;
+	int temp; /*Temporary location*/
+	int i;/*Starting point*/
+	int j;/*End point*/
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
+	j = n - 1;/*Eliminating the null terminator*/
+	i = 0;/*Starting point*/
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+	while (i < j)
+	{
+		temp = *(a + i);/*storing array temporarily*/
+		*(a + i) = *(a + j);/*Swapping values*/
+		*(a + j) = temp;/*Returning reversed array to a*/
+		i++;
+		j--;
+	}
 }
