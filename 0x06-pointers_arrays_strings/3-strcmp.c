a69f6cd796825f611dd9837647454ee397aaff51
Your Name <you@example.com>
2022-09-22 18:49:25 +0300

    comparing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strcmp - compares two strings s1 and s2
+ * @s1: first string
+ * @s2: second string
+ * Return: 0(always success)
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
+	while (*s1 != '\0' || *s2 != '\0')/*Not at the end of either string*/
+	{
+		if (*s1 != *s2)/*Comparing the strings*/
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+
 	return (0);
 }
