57cf8b909ca8d815fd2f9fd25b582cad14f96ac4
Your Name <you@example.com>
2022-09-22 18:53:58 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,22 @@
-int j;
+#include "main.h"
+#include <stdio.h>
 
-	i = 0;
-	while (*(dest + i) != '\0')
-	{
-		i++;
-	}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-	j = 0;
-	while (*(src + j) != '\0')
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
+}
